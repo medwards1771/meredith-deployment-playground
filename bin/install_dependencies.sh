0477edc4798a3df097568595db14c891ddf4538d
Meredith Edwards <meredith.edwards1771@gmail.com>
2024-06-02 15:59:51 -0400

    Serve flask app with gunicorn and nginx

--- bin/install_dependencies.sh
+++ bin/install_dependencies.sh
@@ -9,6 +9,15 @@ set -euxo pipefail
 # Set SSH_AUTH_SOCK so that ssh can find the buildkite-agent bind address
 export SSH_AUTH_SOCK=/var/lib/buildkite-agent/.ssh/ssh-agent.sock
 
-echo "Deploy changes to production"
+scp -r requirements.txt ubuntu@ec2-18-117-132-196.us-east-2.compute.amazonaws.com:meredith-deployment-playground/
 
-scp -r flaskr ubuntu@ec2-18-117-132-196.us-east-2.compute.amazonaws.com:meredith-deployment-playground/
+echo "You are logged in as: $(whoami)"
+echo "The current working directory is: $PWD"
+
+ssh ubuntu@ec2-18-117-132-196.us-east-2.compute.amazonaws.com << 'EOF'
+cd meredith-deployment-playground
+python3 -m venv .venv
+# shellcheck source=/dev/null
+source .venv/bin/activate
+python3 -m pip install -r requirements.txt
+EOF
