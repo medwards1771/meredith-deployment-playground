96bb6244dcaee05e430623af035feba97febb150
Meredith Edwards <meredith.edwards1771@gmail.com>
2024-06-09 11:08:22 -0400

    Remove redundant file
    I'm not installing flask and activating the venv as part of the
    install_dependencies.sh script run in buildkite step "Install dependencies"

--- bin/install_dependencies.sh
+++ bin/install_dependencies.sh
@@ -33,3 +33,25 @@ source .venv/bin/activate
 echo "Install flask app requirements with pip"
 pip install -r requirements.txt
 EOF
+
+
+# I ended up getting this to "work" by following Kenneth Roberts's comment from 10/29/23:
+# "I can never get this to work through sock file. I can get it to work like so without sock file:
+# proxy_pass <http://127.0.0.1:3000≥;" (I changed this to 0.0.0.0:3000)
+# is it insecure to rely on 0.0.0.0 instead of sock?
+
+# To try and resolve "sock file not found blah blah error", I ran the command
+# `sudo chmod 755 /home/ubuntu`
+
+# To get gunicorn to run w/o error from web server, I needed to manually activate virtual environment
+# ssh'd into web server, then from meredith-deploy-playground ran
+#    `. .venv/bin/activate`
+#    `.venv/bin/gunicorn --workers 3 --bind unix:flaskr.sock -m 007 flaskr:app
+
+# Had to manually create a /tmp directory to put the systemd file to avoid changing root-level
+# permissions at /home/ubuntu or /etc/systemd/system
+
+# Had to manually update nginx config (https://flask.palletsprojects.com/en/2.3.x/deploying/nginx/) at
+# /etc/nginx/conf.d/default.conf
+
+# Had to manually add dir meredith-deploy-playground in order to nest flaskr dir within that
